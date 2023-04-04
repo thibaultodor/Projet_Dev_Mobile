2bdbde99b4b08184f91561ede7eebb300399486b
thibault <thibault.odor@outlook.com>
2023-04-05 00:56:37 +0200

    Merge push

---
+++ app/src/main/java/com/example/projet_dev_mobile/ConnectionScreen.java
@@ -0,0 +1,14 @@
+package com.example.projet_dev_mobile;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ConnectionScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.connection_screen);
+    }
+}
---
+++ app/src/main/java/com/example/projet_dev_mobile/SecurityCodeSend.java
@@ -0,0 +1,14 @@
+package com.example.projet_dev_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecurityCodeSend extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.security_code_send);
+    }
+}
